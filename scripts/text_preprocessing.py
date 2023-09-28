a85ea5a0b99f7f341fd0800ca5c1ae2b8879da79
Mario Rosas <mario.rosas@sopristec.com>
2023-09-28 03:00:06 +0200

    adding lemmatization process

--- scripts/text_preprocessing.py
+++ scripts/text_preprocessing.py
@@ -3,13 +3,17 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
 from typing import List, Dict, Tuple
 
+# Spacy imports 
 import spacy
 nlp = spacy.load('en_core_web_sm')
 
+# TextServer imports
 from complementary_material.textserver import TextServer
 
+# NLTK imports
 import nltk
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
 class TextPreprocessing():
 
@@ -18,11 +22,15 @@ class TextPreprocessing():
                  ):
         self.data = data
         self.tokenized_data = []
+        self.lemmatized_data = []
+        self.tag_conversor = {'NN': 'n', 'NNS': 'n', 'JJ': 'a', 'JJR': 'a', 'JJS': 'a', 
+                              'VB': 'v', 'VBD': 'v', 'VBG': 'v', 'VBN': 'v', 'VBP': 'v', 
+                              'VBZ': 'v', 'RB': 'r', 'RBR': 'r', 'RBS': 'r'}
 
     def __len__(self):
         return len(self.data)
 
-    def tokenize_data(self, data = False, method='nltk'):
+    def tokenize_data(self, data=False, method='nltk'):
         ts = TextServer('user', 'passwd', 'tokenizer') 
         self.tokenized_data = []
         t_data = self.data if not data else data
@@ -35,5 +43,27 @@ class TextPreprocessing():
             elif method == 'textserver':
                 self.tokenized_data.append(ts.tokenizer(sentence))
         return self.tokenized_data
+
+    def lemmatize_data(self, data=False, method='nltk'):
+        ts = TextServer('user', 'passwd', 'tokenizer')
+        self.lemmatized_data = []
+        t_data = self.data if not data else data
+        for sentence in t_data:
+            if method == 'nltk':
+                if not isinstance(sentence, list):
+                    print('Applying NLTK tokenization to the sentence')
+                    sentence = nltk.word_tokenize(sentence)
+                tagged_pairs = nltk.pos_tag(sentence)
+                lemmatization = lambda pair:wnl.lemmatize(pair[0], pos=self.tag_conversor[pair[1]]) if pair[1] in self.tag_conversor else pair[0]
+                self.lemmatized_data.append([lemmatization(pair) for pair in pairs])
+            elif method == 'spacy':
+                doc = nlp(sentence)
+                self.lemmatized_data.append([token.lemma_ for token in doc])
+            elif method == 'textserver':
+                ts = TextServer('usuari', 'passwd', 'morpho')
+                self.lemmatized_data([token[1] for token in ts.morpho(sentence)[0]])
+        return self.lemmatized_data
+
+
 
 
