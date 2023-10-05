dd2bfebb503f04094715010fa14801cf9292d7bd
AlamLF <69701984+AlamLF@users.noreply.github.com>
2023-10-05 02:48:08 -0600

    Update models.py

--- scripts/models.py
+++ scripts/models.py
@@ -24,7 +24,9 @@ class StatisticalModels():
         self.amount_data = amounts_data
         self.test_n = test_n
         self.models = models if isinstance(models,dict) else {m:[] for m in models}
-        self.times = {key:[] for key in self.models}
+        self.times_train = {key:[] for key in self.models}
+        self.times_test = {key:[] for key in self.models}
+        self.times_total = {key:[] for key in self.models}
         self.total_results = {key:[] for key in self.models}
         self.a_models = {'HMM':[nltk.tag.hmm.HiddenMarkovModelTrainer, {},[]],
                          'TnT':[nltk.tag.tnt.TnT, {},[]],
@@ -39,18 +41,25 @@ class StatisticalModels():
             train_data = self.data[:i]
 
             for model in self.models.keys():
-                time_before = time.time()
+                time_before_total = time.time()
+                time_before_train = time.time()
                 mod = self.a_models[model][0](**self.a_models[model][1])
                 if model != 'HMM':
                     mod.train(train_data, *self.a_models[model][2])
                 else:
                     mod = mod.train_supervised(train_data, *self.a_models[model][2])
+                self.times_train[model].append(time.time() - time_before_train)
+
+                time_before_test=time.time()
                 self.total_results[model].append(round(mod.accuracy(test_data), 3))
-                self.times[model].append(time.time() - time_before)
+                self.times_test[model].append(time.time() - time_before_test)
+                self.times_total[model].append(time.time() - time_before_total)
+                
+               
 
             print(i)
 
-        return self.times, self.total_results
+        return self.times_train, self.times_test, self.times_total, self.total_results
     
     def results(self):
         df = pd.DataFrame.from_dict(self.total_results)
