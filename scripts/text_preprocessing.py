0d7f14ad95ad6caeba8981541ca507c92a6f9fa0
Mario Rosas <mario.rosas@sopristec.com>
2023-10-28 03:08:32 +0200

    adding most frequent synset data

--- scripts/text_preprocessing.py
+++ scripts/text_preprocessing.py
@@ -16,8 +16,13 @@ import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('treebank')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+nltk.download('wordnet_ic')
+from nltk.corpus import wordnet as wn
+from nltk.corpus import wordnet_ic
+brown_ic = wordnet_ic.ic('ic-brown.dat')
 wnl = nltk.stem.WordNetLemmatizer()
 
 class TextPreprocessing():
@@ -28,6 +33,7 @@ class TextPreprocessing():
         self.data = data
         self.tokenized_data = []
         self.lemmatized_data = []
+        self.mfs_data = []
         self.wsd_lesk_applied_data = []
         self.cleaned_data = []
         self.tag_conversor = {'NN': 'n', 'NNS': 'n', 'NNP':'n', 'NNPS':'n', 
@@ -37,6 +43,33 @@ class TextPreprocessing():
 
     def __len__(self):
         return len(self.data)
+
+    def clean_data(self, data = False, auto = True, lowercase = False, stopwords = False, minwords_len = False, signs = False):
+        self.cleaned_data = []
+        t_data = self.data if not data else data
+        if auto:
+            lowercase = True
+            stopwords=set(nltk.corpus.stopwords.words('english'))
+            signs = string.punctuation
+            minwords_len = 2
+            for element in t_data:
+                self.cleaned_data.append(self.clean_sentence(element, lowercase, stopwords, minwords_len, signs))
+        else: 
+            for element in t_data:
+                 self.cleaned_data.append(self.clean_sentence(element, lowercase, stopwords, minwords_len, signs))
+        return self.cleaned_data
+
+    def clean_sentence(self,sentence, lowercase = True, stopwords = False, minwords_len = False, signs = False):
+        sentence = sentence.split(' ')
+        if lowercase:
+            sentence = [word.lower() for word in sentence]
+        if signs:
+            sentence = [word if not any(caracter in signs for caracter in word) else self.remove_signs(word, signs) for word in sentence]
+        if stopwords:
+            sentence = [word for word in sentence if word not in stopwords and word.isalpha()]
+        if minwords_len:
+            sentence = [word for word in sentence if len(word) > minwords_len]
+        return sentence
 
     def tokenize_data(self, data=False, method='nltk'):
         ts = TextServer('user', 'passwd', 'tokenizer') 
@@ -73,6 +106,36 @@ class TextPreprocessing():
                 self.lemmatized_data([token[1] for token in ts.morpho(sentence)[0]])
         return self.lemmatized_data
 
+    def most_frequent_synset_data(self, data=False, verbose=True):
+        self.mfs_data = []
+        t_data = self.data if not data else data
+        for sentence in t_data:
+            if not isinstance(sentence, list):
+                if verbose:
+                    print('Applying NLTK tokenization to the sentence')
+                sentence = nltk.word_tokenize(sentence)
+            self.mfs_data.append(self.most_frequent_synset_sentence(sentence))
+        return self.mfs_data
+
+    def most_frequent_synset_sentence(self, sentence, include_no_pos=False, false=False):
+        sentence = nltk.pos_tag(sentence)
+        result_sentence = []
+        for (word, tag) in range(sentence):
+            if tag in self.tag_conversor.keys():
+                if len(wn.synsets(word, self.tag_conversor[tag])) > 0:
+                    result_sentence.append(most_frequent_synset(word,sel.tag_conversor[tag]))
+                elif include_no_cat_synsets and len(wn.synsets(word)) > 0:
+                    result_sentence.append(most_frequent_synset(word))
+            elif include_no_pos and len(wn.synsets(word)) > 0:
+                result_sentence.append(most_frequent_synset(word))
+        return result_sentence
+
+            if len(wn.synsets(word))>0:
+                    common= 
+                    tupple=(Lemmas[pair][0],common)
+                    results['accepted'].append(tupple)
+        return result_sentence
+
     def wsd_lesk_data(self, data=False, method='nltk', verbose = True, keep_failures = False, synset_word=False):
         self.wsd_lesk_applied_data = []
         t_data = self.data if not data else data
@@ -96,36 +159,20 @@ class TextPreprocessing():
             return [syns[1] if syns[1] else syns[0] for syns in disambiguated_sentence] 
         return [syns[1] for syns in disambiguated_sentence if syns[1]]
 
-    def clean_data(self, data = False, auto = True, lowercase = False, stopwords = False, minwords_len = False, signs = False):
-        self.cleaned_data = []
-        t_data = self.data if not data else data
-        if auto:
-            lowercase = True
-            stopwords=set(nltk.corpus.stopwords.words('english'))
-            signs = string.punctuation
-            minwords_len = 2
-            for element in t_data:
-                self.cleaned_data.append(self.clean_sentence(element, lowercase, stopwords, minwords_len, signs))
-        else: 
-            for element in t_data:
-                 self.cleaned_data.append(self.clean_sentence(element, lowercase, stopwords, minwords_len, signs))
-        return self.cleaned_data
-
-    def clean_sentence(self,sentence, lowercase = True, stopwords = False, minwords_len = False, signs = False):
-        sentence = sentence.split(' ')
-        if lowercase:
-            sentence = [word.lower() for word in sentence]
-        if signs:
-            sentence = [word if not any(caracter in signs for caracter in word) else self.remove_signs(word, signs) for word in sentence]
-        if stopwords:
-            sentence = [word for word in sentence if word not in stopwords and word.isalpha()]
-        if minwords_len:
-            sentence = [word for word in sentence if len(word) > minwords_len]
-        return sentence
-
     @staticmethod
     def remove_signs(wrd,signs):
         wrd = list(wrd)
         wrd = [word for word in wrd if not any(caracter in signs for caracter in word)]
         wrd = ''.join(wrd)
         return wrd
+
+    @staticmethod
+    def most_frequent_synset(word, pos=None):
+        syns = wn.synsets(word, pos)
+        m = 0
+        res = None
+        for s in syns:
+            for l in s.lemmas():
+                if l.count() > m:
+                    res = l
+        return res.synset()
